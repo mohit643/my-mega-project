354f7b74d98385e37fae68e8dfea71478f106d2e
Mohit sahu@DESKTOP-NSE9EHD <sahumohit643@gmail.com>
2023-09-27 23:45:54 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,23 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("kk",process.env.REACT_APP_TITLE);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <h5 className='bg-slate-400	 p-4'>hello</h5>
+  </>
   );
 }
 
---
+++ src/Conflict/Conflict.js
@@ -0,0 +1,9 @@
+const Conflict = {
+    REACT_APP_TITLE : String(process.env.REACT_APP_TITLE),
+    REACT_APP_PROJECT_ID : String(process.env.REACT_APP_PROJECT_ID),
+    REACT_APP_DATABASE_ID : String(process.env.REACT_APP_DATABASE_ID),
+    REACT_APP_COLLECTION_ID : String(process.env.REACT_APP_COLLECTION_ID),
+    REACT_APP_BUCKET_ID : String(process.env.REACT_APP_BUCKET_ID),
+}
+
+export default Conflict
